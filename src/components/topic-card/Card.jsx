9f56eba9ab683d03979278223d7c0abbb09400be
Emdadul Islam <bekon1700@gmail.com>
2022-10-10 23:46:42 +0600

    topic page done 2

--- src/components/topic-card/Card.jsx
+++ src/components/topic-card/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ info }) => {
     return (
         <div className='card-zoom'>
             <div className="card-zoom-image flex justify-center items-center"
-                style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.8)), url(${info.logo})` }}
+                style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.8)), url(${info.logo})` }}
             >
                 <p className='card-zoom-text '>{info.name}</p>
                 {/* text-2xl lg:text-4xl italic font-bold text-white text-center */}
--- src/pages/topics/Topic.jsx
+++ src/pages/topics/Topic.jsx
@@ -7,7 +7,7 @@ const Topic = () => {
   return (
     <div className='py-8 flex flex-col gap-8'>
       <p className='text-3xl text-center font-semibold uppercase'>Exquisite topics are</p>
-      <div className='flex flex-col lg:flex-row w-11/12 lg:w-2/3 mx-auto'>
+      <div className='flex flex-col lg:flex-row items-center w-11/12 lg:w-2/3 mx-auto'>
         {
           data.map(el => <Card key={el.id} info={el} />)
         }
