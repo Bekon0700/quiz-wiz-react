9f3fc75cc85644524ae1e2e8f15af460c3c07672
Emdadul Islam <bekon1700@gmail.com>
2022-10-10 23:40:32 +0600

    topic page done

--- src/components/topic-card/Card.jsx
+++ src/components/topic-card/Card.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const Card = ({info}) => {
-  return (
-    <div>Card</div>
-  )
+const Card = ({ info }) => {
+    console.log(info)
+    return (
+        <div className='card-zoom'>
+            <div className="card-zoom-image flex justify-center items-center"
+                style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.8)), url(${info.logo})` }}
+            >
+                <p className='card-zoom-text '>{info.name}</p>
+                {/* text-2xl lg:text-4xl italic font-bold text-white text-center */}
+            </div>
+        </div>
+    )
 }
 
 export default Card
--- src/pages/topics/Topic.jsx
+++ src/pages/topics/Topic.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
+import Card from '../../components/topic-card/Card'
 
 const Topic = () => {
   const {data} = useLoaderData()
-  console.log(data)
   return (
-    <div>
-      <p>Exquisite topics are</p>
+    <div className='py-8 flex flex-col gap-8'>
+      <p className='text-3xl text-center font-semibold uppercase'>Exquisite topics are</p>
+      <div className='flex flex-col lg:flex-row w-11/12 lg:w-2/3 mx-auto'>
+        {
+          data.map(el => <Card key={el.id} info={el} />)
+        }
+      </div>
     </div>
   )
 }
